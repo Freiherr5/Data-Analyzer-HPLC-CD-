088d95c179a2c8c43640e40b291564bbbc4e46e7
Freiherr5 <wernerfeiler@gmx.de>
2022-10-20 14:14:57 +0200

    version 1.0

--- Control_center.py
+++ Control_center.py
@@ -2,7 +2,6 @@
 All the different executions you can perform are listed here
 """
 import pandas as pd
-import numpy as np
 import IteratorCD as iter
 import SpectrumCD as spec
 import HPLC as hplc
@@ -10,7 +9,7 @@ import HeatCD as heat
 
 
 while True:
-    #innitiation of programm
+    # innitiation of programm
     print("""
     Data Analyzer for HPLC and CD data
     --> please input the given code string in brackets for the desired mode
@@ -34,7 +33,7 @@ while True:
         print("Program is terminated")
         break
 
-    #HPLC
+    # HPLC
     elif str(initial_input).lower() == "hplc_help":
         hplc.HPLC.help()
 
@@ -44,10 +43,10 @@ while True:
         df1 = pd.read_fwf(str(input1))
         df2 = hplc.HPLC(df1)
         normalized_table = df2.clean_graph()
-        normalize_table.to_csv(input2)
+        normalized_table.to_csv(input2)
 
-        frac_ask = output("Do you want to create fraction file? (yes/no) ")
-        if frac_ask.lower() == yes:
+        frac_ask = input("Do you want to create fraction file? (yes/no) ")
+        if frac_ask.lower() is True:
             input3 = input("Set the name of the Fraction file: ")
             frac_norm = df2.clean_frac()
             frac_norm.to_csv(input3)
@@ -58,18 +57,19 @@ while True:
         fraction = 0
         input1 = input("Path of Text file: ")
         input2 = input("Set the name: ")
+        input3 = input("Set the color (red, blue, green, yellow, etc.): ").lower()
         df1 = pd.read_fwf(str(input1))
         df2 = hplc.HPLC(df1)
         normalized_table = df2.clean_graph()
 
-        frac_ask = output("Do you want to add Fractions? (yes/no) ")
-        if frac_ask.lower() == yes:
+        frac_ask = input("Do you want to add Fractions? (yes/no) ")
+        if frac_ask.lower() is True:
             fraction = 1
 
-        hplc.HPLC.hplc_plot(self, df_frac = fraction, set_show=1, set_name=input2)
+        hplc.HPLC.hplc_plot(normalized_table, df_frac=fraction, set_show=1, set_name=input2, set_color=input3)
         print("Task finished!")
 
-    #CD_spec
+    # CD_spec
     elif str(initial_input).lower() == "spec_help":
         spec.SpectrumPlot.help()
 
@@ -79,29 +79,31 @@ while True:
         df1 = pd.read_fwf(str(input1))
         df2 = spec.SpectrumPlot(df1)
         normalized_table = df2.clean_spectrum()
-        normalize_table.to_csv(input2)
+        normalized_table.to_csv(input2)
         print("Task finished!")
 
     elif str(initial_input).lower() == "pg_spec":
         input1 = input("Path of Text file: ")
         input2 = input("Set the name: ")
+        input3 = input("Set the color (red, blue, green, yellow, etc.): ").lower()
         df1 = pd.read_fwf(str(input1))
         df2 = spec.SpectrumPlot(df1)
         df3 = df2.clean_spectrum()
-        spec.SpectrumPlot.spectrum_plot(df3, set_show=0, set_name=str(input2))
+        spec.SpectrumPlot.spectrum_plot(df3, set_show=0, set_name=str(input2), set_color=input3)
         print("Task finished!")
 
-    #CD_heat
-    elif str(initial_input).lower() == "pg_heat":
+    # CD_heat
+    elif str(initial_input).lower() == "heat_help":
         heat.HeatPlot.help()
 
-    elif str(initial_input).lower() == "ft_heat":
+    elif str(initial_input).lower() == "pg_heat":
         input1 = input("Path of Text file: ")
         input2 = input("Set the name: ")
+        input3 = input("Set the color (red, blue, green, yellow, etc.): ").lower()
         df1 = pd.read_fwf(str(input1))
         df2 = heat.HeatPlot(df1)
-        df3= df2.normalize_heat()
-        heat.HeatPlot.plot_heat(df3, set_show=0, set_name=str(input2))
+        df3 = df2.normalize_heat()
+        heat.HeatPlot.plot_heat(df3, set_show=0, set_name=str(input2), set_color=input3)
         print("Task finished!")
 
     elif str(initial_input).lower() == "ft_heat":
@@ -110,10 +112,10 @@ while True:
         df1 = pd.read_fwf(str(input1))
         df2 = heat.HeatPlot(df1)
         normalized_table = df2.normalize_heat()
-        normalize_table.to_csv(input2)
+        normalized_table.to_csv(input2)
         print("Task finished!")
 
-    #Iterator
+    # Iterator
     elif str(initial_input).lower() == "tb_iter":
         input_csv = input("Please type in the path of your .csv table: ")
         data = pd.read_csv(str(input_csv))
