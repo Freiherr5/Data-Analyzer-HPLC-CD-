a64273193cc8335ffdce5ae58a294f82e5662c4f
Freiherr5 <wernerfeiler@gmx.de>
2022-10-25 14:12:30 +0200

    version 1.1

--- HeatCD.py
+++ HeatCD.py
@@ -62,10 +62,39 @@ class HeatPlot:
     def plot_heat(self, set_show=1, set_name="CD_heat", set_directory= str(path) + "/output_graphs/", set_color="r"):
 
         # generating temp50; searches the temperature where 50% of the protein is denaturated
-        df_50 = self.loc[(self["norm_heat"] >= 0.45) & (self["norm_heat"] <= 0.55)]
+        df_50 = self.loc[(self["norm_heat"] >= 0.40) & (self["norm_heat"] <= 0.60)]
+
+        # taking just the mean is to inaccurate
+        """
         df_50_mean = df_50.mean()
         df_50_result_temp = (df_50_mean.iloc[1] * df_50_mean.iloc[0]) / 0.5
         temp50 = round(df_50_result_temp, 2)
+        """
+
+        # Linear regression is necessary to boost accuracy of the 50% protein denaturation value
+        # mean x and y
+        df_50_mean = df_50.mean()
+        xmean = df_50_mean.iloc[0]
+        ymean = df_50_mean.iloc[1]
+
+        # covariance and variance
+        array_var = []
+        v = 0
+        while v <= len(df_50)-1:
+            xycov = (df_50.iloc[v, 0] - xmean) * (df_50.iloc[v, 1] - ymean)
+            xvar = (df_50.iloc[v, 0] - xmean) ** 2
+            array_inter = [xycov, xvar]
+            array_var.append(array_inter)
+            v = v+1
+        df_var = pd.DataFrame(array_var, columns=["xycov", "xvar"])
+
+        # alpha and beta for the term: Y = a + b*X
+        xycov_sum = round(df_var.sum(axis=0)[0], 2)
+        xvar_sum = round(df_var.sum(axis=0)[1], 2)
+        beta = (xycov_sum / xvar_sum)
+        alpha = ymean - (beta * xmean)
+        temp50_pre = (0.5 - float(alpha))/float(beta)
+        temp50 = round(temp50_pre, 2)
 
         # plotting of the graph (singular, not multiple graphs in one figure)
         max_temp = 110
